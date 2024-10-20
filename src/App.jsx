fbb67520a73a88a224557a31eaa0829ce5cd2b79
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-20 23:42:53 +0600

    Watch JSX and Watch CSS Files Added

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Watch from './components/Watch/Watch'
 
 function App() {
   const watches = [
-    {id: 1, name: "Apple watch", price: 25000},
-    {id: 2, name: "Rolex watch", price: 20000},
-    {id: 3, name: "Casio watch", price: 14000}
-  ]
+    { id: 1, name: "Apple Watch Series 8", price: 399.99 },
+    { id: 2, name: "Samsung Galaxy Watch 6", price: 349.99 },
+    { id: 3, name: "Fitbit Versa 4", price: 229.95 },
+    { id: 4, name: "Garmin Venu 2", price: 399.99 },
+    { id: 5, name: "Amazfit GTR 4", price: 199.99 }
+];
 
   return (
     <>
