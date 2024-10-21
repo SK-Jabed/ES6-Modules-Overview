b68048dfd47188997cd844f0cd1e7cd48e0aed33
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-21 15:02:00 +0600

    All Files are Added and Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
 import './App.css'
 import Watch from './components/Watch/Watch'
 
 function App() {
-  const watches = [
-    { id: 1, name: "Apple Watch Series 8", price: 399.99 },
-    { id: 2, name: "Samsung Galaxy Watch 6", price: 349.99 },
-    { id: 3, name: "Fitbit Versa 4", price: 229.95 },
-    { id: 4, name: "Garmin Venu 2", price: 399.99 },
-    { id: 5, name: "Amazfit GTR 4", price: 199.99 }
-];
+  const [watches, setWatches] = useState([]);
+
+  useEffect(() => {
+    fetch("watches.json")
+    .then(res => res.json())
+    .then(data => setWatches(data))
+  }, [])
+
+//   const watches = [
+//     { id: 1, name: "Apple Watch Series 8", price: 399.99 },
+//     { id: 2, name: "Samsung Galaxy Watch 6", price: 349.99 },
+//     { id: 3, name: "Fitbit Versa 4", price: 229.95 },
+//     { id: 4, name: "Garmin Venu 2", price: 399.99 },
+//     { id: 5, name: "Amazfit GTR 4", price: 199.99 }
+// ];
 
   return (
     <>     
